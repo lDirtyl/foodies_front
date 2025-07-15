28866d2622721a5474785ca4c92e3fbda0fae7c6
Serhii Mishovych <serhiimishovych@gmail.com>
2025-07-15 22:19:06 +0200

    Add Footer, Logo, SocialNetworks, Copyright and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-
 import AppRouter from './AppRouter';
 import QuestWidget from './components/QuestWidget/QuestWidget';
-
-
+import StyleGuide from './pages/StyleGuide';
 
 function App() {
   return (
     <>
       <AppRouter />
       <QuestWidget />
+      <StyleGuide />
     </>
   );
 }
