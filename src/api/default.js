20cf305fbece091ea1efb34720c573183d96fb3f
EssenceMaks <makski.wish@gmail.com>
2025-07-17 03:40:40 +0300

    change server

--- src/api/default.js
+++ src/api/default.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseApiUrl = 'http://localhost:3000/api';
+//const baseApiUrl = 'http://localhost:3000/api';
+const baseApiUrl = 'https://foodies-back-1.onrender.com/api';
 
 export default axios.create({
   baseURL: baseApiUrl,
