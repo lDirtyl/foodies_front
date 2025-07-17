167de200dc4dc35095245ea835ea798bf89a35da
EssenceMaks <makski.wish@gmail.com>
2025-07-17 11:26:01 +0300

    debag endpoins  and user_bar

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,93 +1,13 @@
-import clsx from 'clsx';
-import { FaAngleDown } from 'react-icons/fa6';
-import { GoArrowUpRight } from 'react-icons/go';
-import { NavLink } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { ROUTERS, MODALS } from '../../const';
+import { useSelector } from 'react-redux';
 import { selectUser } from '../../redux/auth/selectors';
-import { showModal } from '../../redux/common/slice';
-
+import __UserBar_Dropdown_Profile_or_LogOut from './__UserBar_Dropdown_Profile_or_LogOut';
 import styles from './UserBar.module.css';
-import stylesNavigation from '../styles/navigation.module.css';
 
-const UserBar = ({ theme, contrast = false }) => {
-  const [open, setOpen] = useState(false);
-  const buttonRef = useRef(null);
-  const dispatch = useDispatch();
+const UserBar = () => {
   const user = useSelector(selectUser);
-  const { name, id, avatarURL } = user || {};
-
-  const className = clsx(styles.placeholder, theme && styles[theme]);
-
-  const handleLogout = () => {
-    dispatch(showModal(MODALS.LOGOUT));
-    setOpen(false);
-  };
-
-  const handleOnOpen = () => {
-    setOpen(!open);
-  };
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (buttonRef.current && !buttonRef.current.contains(event.target)) {
-        setOpen(false);
-      }
-    }
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [buttonRef]);
-
   return (
     <div className={styles.wrapper}>
-      <button className={className} onClick={handleOnOpen} ref={buttonRef}>
-        <img
-          className={styles.avatar}
-          src={avatarURL || '/images/default-avatar.jpg'}
-          width={50}
-          height={50}
-          alt="avatar"
-        />
-        <div className={styles.name}>{name || 'User'}</div>
-        <FaAngleDown className={clsx(styles.dropdown, open && styles.active)} />
-
-        <div
-          className={clsx(
-            styles.menu,
-            open && styles.active,
-            contrast && styles.contrast
-          )}
-        >
-          {id && (
-            <NavLink
-              onClick={handleOnOpen}
-              className={clsx(
-                stylesNavigation.link,
-                styles.link,
-                contrast && stylesNavigation.contrast
-              )}
-              to={`${ROUTERS.USER}/${id}`}
-            >
-              PROFILE
-            </NavLink>
-          )}
-          <a
-            className={clsx(
-              stylesNavigation.link,
-              styles.link,
-              contrast && stylesNavigation.contrast
-            )}
-            href="#"
-            onClick={handleLogout}
-          >
-            LOG OUT <GoArrowUpRight />
-          </a>
-        </div>
-      </button>
+      <__UserBar_Dropdown_Profile_or_LogOut user={user} />
     </div>
   );
 };
---
+++ src/components/UserBar/__UserBar_Dropdown_Profile_or_LogOut.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useRef, useEffect } from 'react';
+import clsx from 'clsx';
+import { Link } from 'react-router-dom';
+import styles from './__UserBar_Dropdown_Profile_or_LogOut.module.css';
+import Avatar from '../Avatar/Avatar';
+import LogOutModal from '../LogOutModal/LogOutModal';
+
+const __UserBar_Dropdown_Profile_or_LogOut = ({ user, onProfile, onLogout }) => {
+  const [open, setOpen] = useState(false);
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleToggle = () => setOpen((prev) => !prev);
+  const handleProfile = () => {
+    setOpen(false);
+    onProfile && onProfile();
+  };
+  const handleLogout = () => {
+    setOpen(false);
+    setShowLogoutModal(true);
+  };
+
+  return (
+    <div className={styles.profileDropdownWrapper} ref={dropdownRef}>
+      <button className={clsx(styles.profileButton, '_placeholder_xo6oc_35')} onClick={handleToggle}>
+        <Avatar src={user?.avatarURL} alt={user?.name} size={50} className={styles.avatar_userbar} />
+        <span className={styles.userName}>{user?.name}</span>
+        <svg className={styles.arrowIcon} width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M7 11L11 7M11 7H7M11 7V11" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </button>
+      {open && (
+        <div className={styles.dropdownMenu}>
+          <Link
+  to={user?.id ? `/user/${user.id}` : '/profile'}
+  className={styles.profileMenuItem}
+  onClick={handleProfile}
+  tabIndex={0}
+>
+  PROFILE
+</Link>
+          <button className={styles.logoutMenuItem} onClick={handleLogout}>
+            LOG OUT
+            <svg className={styles.arrowUpRight} width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7 11L11 7M11 7H7M11 7V11" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </button>
+        </div>
+      )}
+      {showLogoutModal && (
+        <LogOutModal isOpen={showLogoutModal} onClose={() => setShowLogoutModal(false)} />
+      )}
+    </div>
+  );
+};
+
+export default __UserBar_Dropdown_Profile_or_LogOut;
