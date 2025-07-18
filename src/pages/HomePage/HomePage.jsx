d8e91a4b3e2345d554e50c94d96c4d15c1f80244
Tanya <tanya.chapaliuk@gmail.com>
2025-07-18 07:50:03 +0300

    Add markup for header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,29 +7,16 @@ import Hero from '../../components/Hero/Hero.jsx'
 export default function HomePage() {
   return (
     <PageWrapper
-      // title="FOODIES"
-      // description="Discover amazing recipes from talented chefs around the world"
-      // containerSize="wide"
       showBreadcrumbs={false}
     >
       {/* Hero Section */}
       <section style={{ marginBottom: '60px' }}>
-        {/*<h3>Featured Recipes</h3>*/}
-        {/*<p>Explore our collection of delicious recipes...</p>*/}
         <Hero />
       </section>
 
       {/* Categories Section */}
       <section style={{ marginTop: '60px' }}>
-        {/* <h3>Popular Categories</h3>
-        <p>Browse recipes by category...</p> */}
         <Categories />
-      </section>
-
-      {/* Latest Recipes */}
-      <section style={{ marginTop: '60px' }}>
-        <h3>Latest Recipes</h3>
-        <p>Check out the newest additions to our recipe collection...</p>
       </section>
 
       <Testimonials />
